59c75cf3a62e520859f3663e163c61df79921d21
wellcheck <wellington.check@gmail.com>
2023-06-08 16:01:28 -0300

    Metodos CRUD adicionados ao Controller

--- src/main/java/com/bidea/bideafactory/controller/ClienteController.java
+++ src/main/java/com/bidea/bideafactory/controller/ClienteController.java
@@ -1,2 +1,25 @@
-package com.bidea.bideafactory.controller;public class ClienteController {
+package com.bidea.bideafactory.controller;
+
+import com.bidea.bideafactory.entities.Cliente;
+import com.bidea.bideafactory.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/books")
+public class ClienteController {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @GetMapping()
+    public List<Cliente> findAll(){
+        var result = clienteService.findAll();
+        return result;
+    }
 }
--- src/main/java/com/bidea/bideafactory/service/ClienteService.java
+++ src/main/java/com/bidea/bideafactory/service/ClienteService.java
@@ -1,2 +1,20 @@
-package com.bidea.bideafactory.service;public class ClienteService {
+package com.bidea.bideafactory.service;
+
+import com.bidea.bideafactory.entities.Cliente;
+import com.bidea.bideafactory.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public List<Cliente> findAll(){
+        List<Cliente> result = clienteRepository.findAll();
+        return result;
+    }
 }
